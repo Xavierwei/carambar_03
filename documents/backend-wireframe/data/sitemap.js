caef16af5327e36df064aa86b3c0bd9cb224322c
tonysh518 <tony@fuel-it-up.com>
2014-03-05 22:34:56 +0800

    updatea

--- documents/backend-wireframe/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k,i,[_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q)]),_(c,r,e,f,g,s,i,[_(c,p,e,f,g,t)]),_(c,u,e,f,g,v),_(c,w,e,f,g,x,i,[_(c,y,e,f,g,z),_(c,A,e,f,g,B),_(c,p,e,f,g,C),_(c,D,e,f,g,E)]),_(c,F,e,f,g,G,i,[_(c,j,e,f,g,H),_(c,r,e,f,g,I)])])]);}; 
-var b="rootNodes",c="pageName",d="Home",e="type",f="Wireframe",g="url",h="Home.html",i="children",j="Contents",k="Contents.html",l="Video",m="Video.html",n="Photo",o="Photo.html",p="Edit",q="Edit.html",r="Comments",s="Comments.html",t="Edit_1.html",u="Like",v="Like.html",w="User",x="User.html",y="Editor",z="Editor.html",A="Admin",B="Admin.html",C="Edit_2.html",D="Create New User",E="Create_New_User.html",F="Flag",G="Flag.html",H="Contents_1.html",I="Comments_1.html";
-return _creator();
-})();
