1057b224ab51418caa51eb4f27514d9035d05127
tonysh518 <tony@fuel-it-up.com>
2014-03-15 02:03:17 +0800

    update

--- js/lp.index.js
+++ js/lp.index.js
@@ -123,7 +123,74 @@ LP.use(['jquery', 'api', 'easing'] , function( $ , api ){
                     $('#twitter-content-wrap').prev().addClass('opened')
                 }
 			}
-		})
+		});
+
+
+
+
+
+
+		/* for animation */
+		var isUglyIe = false; //TODO
+		var ANIMATE_NAME = "data-animate";
+		$('[' + ANIMATE_NAME + ']')
+			.each(function(){
+				var $dom = $(this);
+				var tar = $dom.data('animate');
+				var browser = $dom.data('browser');
+				var style = $dom.data('style');
+				var time = parseInt( $dom.data('time') );
+				var delay = $dom.data('delay') || 0;
+				var easing = $dom.data('easing');
+				var begin = $dom.data('begin');
+				tar = tar.split(';');
+				var tarCss = {} , tmp;
+				if(browser == 'uglyie' && isUglyIe) {
+					return;
+				}
+				for (var i = tar.length - 1; i >= 0; i--) {
+					tmp = tar[i].split(':');
+					if( tmp.length == 2 )
+						tarCss[ tmp[0] ] = $.trim(tmp[1]);
+				}
+				if( isUglyIe && tarCss.opacity !== undefined ){
+					delete tarCss.opacity;
+				}
+
+
+				style = style.split(';');
+				var styleCss = {} , tmp;
+				for (var i = style.length - 1; i >= 0; i--) {
+					tmp = style[i].split(':');
+					if( tmp.length == 2 )
+						styleCss[ tmp[0] ] = $.trim(tmp[1]);
+				}
+				if( isUglyIe && styleCss.opacity !== undefined ){
+					delete styleCss.opacity;
+				}
+				$dom.css(styleCss).delay( delay )
+					.animate( tarCss , time , easing );
+				if( begin ){
+					setTimeout(function(){
+						animation_begins[begin].call( $dom );
+					} , delay);
+				}
+			});
+
+		var timeoffset = isUglyIe?0:2000;
+//		setTimeout(function(){
+//			$('.section,.ball_round').show();
+//			s = skrollr.init({
+//				smoothScrollingDuration:0,
+//				smoothScrolling:true,
+//				easing:'outCubic',
+//				scale:10
+//			});
+//			if(getQueryString('pos') == 'home'){
+//				s.animateTo(1571*skrollr_scale);
+//			}
+//
+//		},timeoffset);
 
 
 	}
