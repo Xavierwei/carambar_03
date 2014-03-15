0645cf41e8951337438ec794c19bed2a5c2001cb
tonysh518 <tony@fuel-it-up.com>
2014-03-15 23:40:52 +0800

    update t/l

--- protected/views/layouts/index.php
+++ protected/views/layouts/index.php
@@ -1,80 +1,45 @@
-
-<!DOCTYPE html>
-<!--[if IE 6]>
-<html class="ie6"><![endif]-->
-<!--[if IE 7]>
-<html class="ie7"><![endif]-->
-<!--[if IE 8]>
-<html class="ie8"><![endif]-->
-<!--[if IE 9]>
-<html class="ie9"><![endif]-->
-<!--[if IE 10]>
-<html class="ie10"><![endif]-->
-<!--[if (gte IE 11)|(gt IEMobile 7)]><!-->
-<html>
-<!--<![endif]-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
-	<title>Carambar</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=11; IE=10; IE=9; IE=8" />
-	<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1, user-scalable=no" />
-	<link href="css/index_style.css" rel="stylesheet" type="text/css" />
-	<link href="css/animation.css" rel="stylesheet" type="text/css" />
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+	<title></title>
+	<link href="./css/style.css" rel="stylesheet" type="text/css" />
 	<style>
-		.vote-result {color:#fff;}
-		.vote_box {cursor:pointer;float: left;width:100px;height:40px;line-height:40px;background: #fff;margin:10px;text-align: center;}
-		#countdown {padding-top:50px;display: block;color:#fff;clear: both;font-size:40px;}
-		#countdown div {float: left;margin:0 10px;}
-		.facebook-wrap {display: ;clear: both;padding:50px 0 0;}
-		.facebook-wrap .content-box {display: none;}
-		.twitter-wrap {display: ;clear: both;padding:50px 0 0;}
-		.twitter-wrap .content-box {display: none;}
-		.vote-result {clear:both;font-size:40px;clear:both;padding:50px 0 0;}
+		#stats {z-index: 99999;position: fixed;}
 	</style>
 </head>
 <body>
-	<div class="header">
-		<div class="hdcontent">
-			<a class="nav1 nav1on" href="#"></a>
-			<a class="nav2" href="#"></a>
-		</div>
-	</div>
-
-	<div class="page">
-		<?php echo $content; ?>
-	</div>
-
-	<!-- footer -->
-	<div class="footer">
-		Mentions légales
-	</div>
-
-	<!--  -->
-	<div class="fixed">
-		<p class="fixed-1">lalalalal</p>
-		<p class="fixed-2">lalalalallalalalal</p>
-		<p class="fixed-3">lalalalallalalalal</p>
-	</div>
-	<!--  -->
-
-	<!-- vote-result-tpl -->
-	<script type="text/tpl" id="vote-result-template">
-		<div class="vote-result">
-			{{#each data}}
-			<div>{{votes}} votes</div>
-			{{/each}}
-		</div>
-	</script>
-
-
+<?php echo $content;?>
 
 <script type="text/javascript" src="./js/plugin/modernizr-2.5.3.min.js"></script>
 <script type="text/javascript" src="./js/sea/sea-debug.js" data-config="../config.js"></script>
 <script type="text/javascript" src="./js/sea/plugin-shim.js"></script>
 <script type="text/javascript" src="./js/lp.core.js"></script>
 <script type="text/javascript" src="./js/lp.index.js"></script>
+<script type="text/javascript" src="./js/plugin/state.js"></script>
+<script type="text/javascript">
+	var stats = new Stats();
+	stats.setMode(0); // 0: fps, 1: ms
 
+	// Align top-left
+	stats.domElement.style.position = 'fixed';
+	stats.domElement.style.left = '0px';
+	stats.domElement.style.top = '0px';
+
+	document.body.appendChild( stats.domElement );
+
+	setInterval( function () {
+
+		stats.begin();
+
+		// your code goes here
+
+		stats.end();
+
+	}, 1000 / 60 );
+</script>
 </body>
 </html>
