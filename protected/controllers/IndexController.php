e7a4b977b6249b40c416005d99de0bde0273f70d
drogjh <drogjh@gmail.com>
2014-03-20 10:46:30 +0800

    fix index/index

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -23,14 +23,14 @@ class IndexController extends Controller {
 		$phase = isset($phase) ? $phase : 1;
 
 		// get top video
-		$topVideo = VideoAR::model()->getVideoList(1,1,1,1);
+		$topVideo = VideoAR::model()->getVideoList(1,1,1,1,$phase);
 
 		// get home video
 		$homeVideosNum = 3;
 		if($phase == 5) {
 			$homeVideosNum = 5;
 		}
-		$homeVideos = VideoAR::model()->getVideoList(null,1,$homeVideosNum,1);
+		$homeVideos = VideoAR::model()->getVideoList(null,1,$homeVideosNum,1,$phase);
 
 		$this->render('index', array(
 			'topVideo'		=> $topVideo[0],
